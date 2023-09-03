279f79c8e942d7208810038decca858b64cd1d9d
Mai Cường Thọ <thomc@ntu.edu.vn>
2023-09-03 15:40:46 +0700

    Updload new

--- MyToolbarAsActionbar/app/src/main/java/com/maicuongtho/mytoolbarasactionbar/MainActivity.java
+++ MyToolbarAsActionbar/app/src/main/java/com/maicuongtho/mytoolbarasactionbar/MainActivity.java
@@ -3,6 +3,10 @@ package com.maicuongtho.mytoolbarasactionbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         // using toolbar as ActionBar
         setSupportActionBar(toolbar);
+        // Display application icon in the toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.app_icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        // assigning ID of textView2 to a variable
+        ImageView img2= (ImageView)findViewById(R.id.imageView2);
+        img2.setClickable(true);
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),
+                        "You clicked on Build Icon",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
